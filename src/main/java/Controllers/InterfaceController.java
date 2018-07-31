cea0f8941559d4767478a4e6a1aa5ab19e0156eb
zzh <zzh@zzh-HP>
2018-07-31 22:50:27 +0800

    other

--- src/main/java/Controllers/InterfaceController.java
+++ src/main/java/Controllers/InterfaceController.java
@@ -36,12 +36,17 @@ public class InterfaceController {
 			else {
 				list.add(service.selectByName(interfacename));
 				model.addAttribute("one_list", list);
+				System.out.println();
 				return "queryone";
 			}
 		}
 	}
 
-	// ɾ��
+	public void say() {
+		System.out.println("");
+	}
+
+	// 删锟斤拷
 	@RequestMapping("del")
 	public String delete(@RequestParam(required = false) String interface_id, HttpServletRequest request, Model model) {
 		if (interface_id == null) {
@@ -52,7 +57,7 @@ public class InterfaceController {
 		}
 	}
 
-	// ������ת
+	// 锟斤拷锟斤拷锟斤拷转
 	@RequestMapping("jump")
 	public String jump(@RequestParam(required = false) String interface_id, HttpServletRequest request, Model model) {
 		if (interface_id == null) {
@@ -65,61 +70,60 @@ public class InterfaceController {
 		}
 	}
 
-	// ������ת
+	// 锟斤拷锟斤拷锟斤拷转
 	@RequestMapping("jump_add")
 	public String jump2(@RequestParam(required = false) String interface_id, HttpServletRequest request, Model model) {
 		return "add";
 	}
 
-	// ����
+	// 锟斤拷锟斤拷
 	@RequestMapping("add")
 	public String add(MyInterface my, HttpServletRequest request, Model model) {
-		String id = my.getId();// ����id��
-		String name = my.getName();// ��������
-		String requestMethod = my.getRequestMethod();// ��������ģʽ
-		String requestParameters = my.getRequestParameters();// �����������
-		String responseParameter = my.getResponseParameter();// ������Ӧ����
-		String classification = my.getClassification();// �������
-		//У�鲻��Ϊ��
+		String id = my.getId();// 锟斤拷锟斤拷id锟斤拷
+		String name = my.getName();// 锟斤拷锟斤拷锟斤拷锟斤拷
+		String requestMethod = my.getRequestMethod();// 锟斤拷锟斤拷锟斤拷锟斤拷模式
+		String requestParameters = my.getRequestParameters();// 锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟�
+		String responseParameter = my.getResponseParameter();// 锟斤拷锟斤拷锟斤拷应锟斤拷锟斤拷
+		String classification = my.getClassification();// 锟斤拷锟斤拷锟斤拷锟�
+		// 校锟介不锟斤拷为锟斤拷
 		if (id.equals("") || name.equals("") || requestMethod.equals("") || requestParameters.equals("")
 				|| responseParameter.equals("") || classification.equals("")) {
 			return "wrong";
 		} else {
-			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// �������ڸ�ʽ
+			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// 锟斤拷锟斤拷锟斤拷锟节革拷式
 			my.setTime(df.format(new Date()).toString());
 			service.insert(my);
 			return "success";
 		}
 	}
 
-	// �޸�
+	// 锟睫革拷
 	@RequestMapping("modify_interface")
-	public String modify_interface(MyInterface my, HttpServletRequest request,
-			Model model) {
-		String id = my.getId();// ����id��
-		String name = my.getName();// ��������
-		String requestMethod = my.getRequestMethod();// ��������ģʽ
-		String requestParameters = my.getRequestParameters();// �����������
-		String responseParameter = my.getResponseParameter();// ������Ӧ����
-		String classification = my.getClassification();// �������
-		//У�鲻��Ϊ��
+	public String modify_interface(MyInterface my, HttpServletRequest request, Model model) {
+		String id = my.getId();// 锟斤拷锟斤拷id锟斤拷
+		String name = my.getName();// 锟斤拷锟斤拷锟斤拷锟斤拷
+		String requestMethod = my.getRequestMethod();// 锟斤拷锟斤拷锟斤拷锟斤拷模式
+		String requestParameters = my.getRequestParameters();// 锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟�
+		String responseParameter = my.getResponseParameter();// 锟斤拷锟斤拷锟斤拷应锟斤拷锟斤拷
+		String classification = my.getClassification();// 锟斤拷锟斤拷锟斤拷锟�
+		// 校锟介不锟斤拷为锟斤拷
 		if (id.equals("") || name.equals("") || requestMethod.equals("") || requestParameters.equals("")
 				|| responseParameter.equals("") || classification.equals("")) {
 			return "wrong";
 		} else {
-			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// �������ڸ�ʽ
+			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// 锟斤拷锟斤拷锟斤拷锟节革拷式
 			my.setTime(df.format(new Date()).toString());
 			service.updateById(my);
 			return "success";
 		}
 	}
 
-	// ��ѯ����
+	// 锟斤拷询锟斤拷锟斤拷
 	@RequestMapping(value = "queryAll")
 	public String showCandida(@RequestParam(required = false) String candida, HttpServletRequest request, Model model) {
 		ArrayList<MyInterface> list = (ArrayList<MyInterface>) service.selectAll();
 		model.addAttribute("c_list", list);
-		System.out.println("��ѯ�ɹ�");
+		System.out.println("锟斤拷询锟缴癸拷");
 		return "queryshow";
 	}
 
